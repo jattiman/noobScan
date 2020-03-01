17929dea34362a0faf2aabe613cd3850e0ae6095
John Atti <thejohnatti@gmail.com>
2020-03-01 17:37:37 -0500

    Initial Commit

---
+++ noobScan/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  noobScan
+//
+//  Created by John Atti on 3/1/20.
+//  Copyright © 2020 John Atti. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+   return 0;
+}
