9852317c25a624784f6a487aaf9b8ff81905ec9d
John Atti <thejohnatti@gmail.com>
2020-03-02 10:31:41 -0500

    Confirming code commit. Nothing of note added.

---
+++ noobScan/scanaddress.cpp
@@ -0,0 +1,9 @@
+//
+//  scanaddress.cpp
+//  noobScan
+//
+//  Created by John Atti on 3/1/20.
+//  Copyright © 2020 John Atti. All rights reserved.
+//
+
+#include "scanaddress.h"
---
+++ noobScan/scanaddress.h
@@ -0,0 +1,14 @@
+//
+//  scanaddress.hpp
+//  noobScan
+//
+//  Created by John Atti on 3/1/20.
+//  Copyright © 2020 John Atti. All rights reserved.
+//
+
+#ifndef scanaddress_hpp
+#define scanaddress_hpp
+
+#include <stdio.h>
+
+#endif /* scanaddress_hpp */
